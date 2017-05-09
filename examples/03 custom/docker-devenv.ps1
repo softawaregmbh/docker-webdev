9a0c41a4cde41b16e49dc4180ff91ad95a66ce11
Bernhard Mayr <bernhard@mayr.io>
2017-05-09 13:40:50 +0200

    fixed Startup-Scripts

--- examples/03 custom/docker-devenv.ps1
+++ examples/03 custom/docker-devenv.ps1
@@ -1,9 +1,12 @@
 $projectname = (Get-Item -Path ".\" -Verbose).Name
 try { $projectname = (Get-Content -Raw -Path package.json -ErrorAction Stop | ConvertFrom-Json).Name } catch {} # try to get the project name out of package.json
+$projectname = $projectname.Replace(" ", "_")
 $tag = "webdev"
 
 $imagename = $projectname + ":" + $tag
-$containername = $projectname.Replace(" ", "_") + "-" + $tag
+$containername = $projectname + "-" + $tag
 
-docker image build -t $imagename .
+"Creating Image"
+docker image build --quiet -t $imagename . | Out-Null
+"Starting Container"
 docker container run -it --rm -p 4200:4200 -v ${pwd}:/usr/src/app --name ${containername} $imagename
