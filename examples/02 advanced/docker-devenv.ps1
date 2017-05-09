9a35d1ec69ef22f035d5f1989694a1b929a7d634
Bernhard Mayr <bernhard@mayr.io>
2017-05-09 20:20:47 +0200

    fixed container naming

--- examples/02 advanced/docker-devenv.ps1
+++ examples/02 advanced/docker-devenv.ps1
@@ -1,6 +1,6 @@
 $projectname = (Get-Item -Path ".\" -Verbose).Name
 try { $projectname = (Get-Content -Raw -Path package.json -ErrorAction Stop | ConvertFrom-Json).Name } catch {} # try to get the project name out of package.json
-$projectname = $projectname.Replace(" ", "_")
+$projectname = $projectname.Replace(" ", "_").Replace("@", "").Replace("/", "").Replace("\", "")
 $containername = $projectname + "-webdev"
 
 docker container run -it --rm -p 4200:4200 -v ${pwd}:/usr/src/app --name ${containername} softaware/webdev:angular-6.10.3
--- examples/03 custom/docker-devenv.ps1
+++ examples/03 custom/docker-devenv.ps1
@@ -1,6 +1,6 @@
 $projectname = (Get-Item -Path ".\" -Verbose).Name
 try { $projectname = (Get-Content -Raw -Path package.json -ErrorAction Stop | ConvertFrom-Json).Name } catch {} # try to get the project name out of package.json
-$projectname = $projectname.Replace(" ", "_")
+$projectname = $projectname.Replace(" ", "_").Replace("@", "").Replace("/", "").Replace("\", "")
 $tag = "webdev"
 
 $imagename = $projectname + ":" + $tag
